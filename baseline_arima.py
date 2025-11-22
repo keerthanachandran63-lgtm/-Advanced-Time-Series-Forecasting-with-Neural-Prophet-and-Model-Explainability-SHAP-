4aad42f99a6c6689f3795aca995292702a74bb96
keerthanachandran63-lgtm <keerthanachandran63@gmail.com>
2025-11-22 08:37:03 +0530

    Add files via upload

--- baseline_arima.py
+++ baseline_arima.py
@@ -1,10 +1,1 @@
-
-import pandas as pd
-from statsmodels.tsa.arima.model import ARIMA
-
-df = pd.read_csv("dataset.csv")
-model = ARIMA(df['y'], order=(5,1,2))
-fit = model.fit()
-forecast = fit.predict(len(df), len(df)+60)
-
-print("ARIMA forecast complete.")
+print('placeholder arima')
